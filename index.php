da5070a838d67713705aa506596296e2770cd163
devansol <devansol@users.noreply.github.com>
2019-07-20 16:43:06 +0700

    commit

---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Front End Test
+	</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<header>
+		<div class="container logo-menu">
+			<div class="row">
+				<div class="col-xs-4 col-md-4">
+					<img src="">
+				</div>
+				<div class="col-xs-8 col-md-8">
+					<ul class="nav">
+						<li class="nav-item"><a class="nav-link" href="">Hosting</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Domain</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Server</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Website</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Afiliasi</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Promo</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Pembayaran</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Review</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Kontak</a></li>
+						<li class="nav-item"><a class="nav-link" href="">Blog</a></li>
+					</ul>
+
+				</div>
+			</div>
+		</div>
+		<hr/>
+	</header>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-6 col-md-6">
+				<h2>PHP Hosting</h2>
+				<p style="font-size: 22px">Cepat, handal, penuh dengan modul PHP yang anda butuhkan</p>
+				<p><i class="fa fa-check-circle" style="font-size:18px;color: #33cc33"></i> Solusi PHP untuk performa query yang lebih cepat</p>
+				<p><i class="fa fa-check-circle" style="font-size:18px;color: #33cc33"></i> Konsumsi memori lebih rendah</p>
+				<p><i class="fa fa-check-circle" style="font-size:18px;color: #33cc33"></i> Support PHP 5.3, PHP 5.4, PHP 5.5, PHP 5.6, PHP 7</p>
+				<p><i class="fa fa-check-circle" style="font-size:18px;color: #33cc33"></i> Fitur enkripsi IonCube dan Zend Guard Loaders</p>
+			</div>
+
+			<div class="col-xs-6 col-md-6">
+				<img src="assets/svg/illustration banner PHP hosting-01.svg">
+			</div>
+		</div>
+	</div>
+	<hr>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-4 col-md-4">
+				<img src="assets/svg/zendguard.svg">
+				<p>PHP Zend Guard Loader</p>
+			</div>
+
+			<div class="col-xs-4 col-md-4">
+				<img src="assets/svg/composer.svg">
+				<p>PHP Composer</p>
+			</div>
+
+			<div class="col-xs-4 col-md-4">
+				<img src="assets/svg/ioncube.svg">
+				<p>PHP ionCube Loader</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-md-12" style="text-align: center;">
+				<p style="font-weight: bold;font-size: 25px">Paket Hosting Singapura yang Tepat</p>
+				<p style="font-size: 20px">Diskon 40% + Domain dan SSL Gratis untuk Anda</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-md-12" style="text-align: center;">
+				<table class="table table-bordered">
+					<tr>
+						<td>Bayi</td>
+						<td>Pelajar</td>
+						<td>Personal</td>
+						<td>Bisnis</td>
+					</tr>
+					<tr>
+						<td>hargaasli Harga Diskon</td>
+						<td>hargaasli Harga Diskon</td>
+						<td>hargaasli Harga Diskon</td>
+						<td>hargaasli Harga Diskon</td>
+					</tr>
+
+					<tr>
+						<td>938 Pengguna Terdaftar</td>
+						<td>4.168 Pengguna Terdaftar</td>
+						<td>10.017 Pengguna Terdaftar</td>
+						<td>3.552 Pengguna Terdaftar</td>
+					</tr>
+				</table>
+			</div>
+			
+		</div>
+	</div>
+	<hr>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</html>
